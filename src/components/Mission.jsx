e26e014339bc6e7c21867f112b1d922bac961259
coderhanzo <reinerbielert@icloud.com>
2024-06-07 23:16:07 +0000

    fixed card spacing

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -17,12 +17,12 @@ const Mission = () => {
   };
   return (
     <>
-      <div className="p-5 grid grid-cols-3 max-sm:grid-cols-1 ">
-        <div className=" text-black h-[37rem] w-[20rem] shadow-xl rounded-lg max-sm:w-[23rem]  ">
+      <div className="p-5 grid grid-cols-3 gap-5 content-center max-sm:grid-cols-1">
+        <div className="text-black h-[37rem] w-[24rem] shadow-xl rounded-lg max-sm:w-[23rem] mx-auto">
           <div className="flex justify-center">
             <img src={plastic} alt="" className="w-[15rem] max-sm:w-[15rem]" />
           </div>
-          <div className=" px-2">
+          <div className="px-2">
             <h2 className="font-bold text-[23px] text-center">Our Mission</h2>
             <p className="text-center">
               Our mission is to lead in PET recycling, promoting a circular
@@ -34,11 +34,11 @@ const Mission = () => {
             </p>
           </div>
         </div>
-        <div className=" text-black h-[37rem] w-[20rem] shadow-xl rounded-lg hover:scale-105 hover:duration-150  max-sm:w-[23rem]  ">
+        <div className="text-black h-[37rem] w-[24rem] shadow-xl rounded-lg max-sm:w-[23rem] mx-auto hover:scale-105 hover:duration-150">
           <div className="flex justify-center">
             <img src={plastic} alt="" className="w-[15rem] max-sm:w-[15rem]" />
           </div>
-          <div className=" px-2">
+          <div className="px-2">
             <h2 className="font-bold text-[23px] text-center">Our Services</h2>
             <p className="text-center">
               JOMO RECYCLE offers complete PET plastic bottle recycling
@@ -52,18 +52,18 @@ const Mission = () => {
           </div>
           <div className="flex justify-center items-center pt-3">
             <Link to="/services">
-              <button className="btn font-semibold ">Learn More</button>
+              <button className="btn font-semibold">Learn More</button>
             </Link>
           </div>
         </div>
-        <div className=" text-black h-[37rem] w-[20rem] shadow-xl rounded-lg  max-sm:w-[23rem]  ">
+        <div className="text-black h-[37rem] w-[24rem] shadow-xl rounded-lg max-sm:w-[23rem] mx-auto">
           <div className="flex justify-center">
             <img src={plastic} alt="" className="w-[15rem] max-sm:w-[15rem]" />
           </div>
-          <div className=" px-2">
+          <div className="px-2">
             <h2 className="font-bold text-[23px] text-center">Jomo Team</h2>
             <p className="text-center">
-              Our team comprises of experienced professionals dedicated to
+              Our team comprises experienced professionals dedicated to
               sustainability and excellence in PET plastic bottle recycling.
               From leadership and operations to quality control and logistics,
               each member plays a vital role in ensuring the highest standards.
